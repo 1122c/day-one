d0c1df50eecf2d26a466047d5d75730b13b77c16
1122c <118850768+1122c@users.noreply.github.com>
2025-08-26 19:14:11 -0700

    fixed discover page layout

--- src/components/discovery/ProfileDiscovery.tsx
+++ src/components/discovery/ProfileDiscovery.tsx
@@ -369,7 +369,7 @@ export default function ProfileDiscovery({
         </div>
 
         {/* Profile Cards */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
           {filteredProfiles.map((profile) => {
             const compatibilityScore = getCompatibilityScore(profile);
             return (
